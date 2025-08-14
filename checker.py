d9ff1c84a2a87367f4306cd9e422c53f687d458b
bpmo1234 <67483858+bpmo1234@users.noreply.github.com>
2025-08-14 09:17:38 +0000

    Update checker.py

--- checker.py
+++ checker.py
@@ -12,7 +12,7 @@ def check():
     
     driver = Driver(uc=True, incognito=True, headless=True)
 
-    driver.get("https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600")
+    driver.get("https://service2.diplo.de/rktermin/extern/choose_category.do?locationCode=ouag&realmId=795&categoryId=1462")
 
     try:
         elem_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/fieldset/form/div[8]/div[2]/select")))
